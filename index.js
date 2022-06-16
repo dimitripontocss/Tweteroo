ae7ead1af34f5d0962f81472c164845cd3b1efb1
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-06-16 15:21:50 -0300

    Projeto Pronto faltam os bônus

--- index.js
+++ index.js
@@ -3,21 +3,42 @@ import cors from 'cors'
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 
 let users = [];
 let tweets = [];
 
 server.post("/sign-up", (req,res) =>{
-    const newUser = req.body;
-    res.send(newUser);
+    const { body } = req;
+    const newUser = {
+        username: body.username,
+        avatar: body.avatar
+    }
+    users.push(newUser);
+    res.send("OK");
 })
 
 server.post("/tweets", (req,res) =>{
-
+    const { body } = req;
+    const selectedUser = users.find(user => user.username === body.username);
+    tweets.push(
+        {
+          username: body.username,
+          avatar: selectedUser.avatar,
+          tweet: body.tweet  
+        }
+    )
+    res.send("OK");
 })
 
 server.get("/tweets", (req,res) =>{
-
+    let sendableTweets;
+    if(tweets.length > 10){
+        sendableTweets = tweets.slice(tweets.length -10);
+    }else{
+        sendableTweets = tweets;
+    }
+    res.send(sendableTweets);
 })
 
 server.listen(5000)
